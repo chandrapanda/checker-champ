57971fdd62ad914968d6ae38c953261ec9036722
Chandra Holt <caholt@sbgtv.com>
2022-12-29 12:58:35 -0800

    Initial files created. Rendered squares on page

---
+++ README.md
@@ -0,0 +1,41 @@
+CAUTION: This email originated from outside of Sinclair. Do not click links or open attachments unless you recognize the sender and know the content is safe.
+
+# Checker Champ
+
+## Description
+
+This Checkerboard and gamepiece framework will be utilized as an example with abstracted and reusable code for game-frame implementation. Eventually, this game may be playable. For now, I am creating a grid that has pieces that allows implementation of behaviors such as hover events, mouse events, drag and drop, etc.
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Contributing](#contributing)
+4. [Tests](#tests)
+5. [Questions](#questions)
+
+## Installation
+
+N/A
+
+## Usage
+
+This project should be utilized as a springboard for future development of games.
+
+## Contributing
+
+Chandra Holt
+
+## Tests
+
+N/A
+
+## Questions
+
+### GitHub
+
+[GitHub](https://www.github.com/chandrapanda)
+
+### Email
+
+chandra_holt@hotmail.com
---
+++ dist/main.js
@@ -0,0 +1,108 @@
+//grab canvas through variable instantiation
+var canvas = document.querySelector("canvas");
+
+//set canvas to full screen
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//context variable
+var c = canvas.getContext("2d");
+
+//Create rectangles for game board
+// c.fillStyle = "rgba(60, 179, 113, 0.5)";
+// c.fillRect(400, 100, 100, 100);
+// c.fillStyle = "rgba(60, 60, 60, 0.5)";
+// c.fillRect(300, 300, 100, 100);
+
+//instantiate mouse position
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
+});
+
+//object for square on game board
+function Rectangle(x, y, width, height, color) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  //create method within object to draw each square
+  this.draw = function () {
+    c.fillStyle = this.color;
+    c.fillRect(this.x, this.y, this.width, this.height);
+    // c.stroke();
+    // c.fill();
+  };
+
+  //this is where interactivity occurs
+  if (
+    mouse.x - this.xAxis < 50 &&
+    mouse.x - this.xAxis > -50 &&
+    mouse.y - this.yAxis < 50 &&
+    mouse.y - this.yAxis > -50
+  ) {
+    if (this.radius < maxRadius) {
+      this.radius += 1;
+    }
+  } else if (this.radius > this.minRadius) {
+    this.radius -= 1;
+  }
+
+  this.draw();
+}
+
+function init() {
+  for (let i = 0; i < canvas.width; i += 200) {
+    for (let j = 0; j < canvas.height; j += 200) {
+      let rectangle = new Rectangle(i, j, 100, 100, "rgba(60, 179, 113, 0.5)");
+      rectangle.draw();
+    }
+  }
+  for (let i = 100; i < canvas.width; i += 200) {
+    for (let j = 100; j < canvas.height; j += 200) {
+      let rectangle = new Rectangle(i, j, 100, 100, "rgba(60, 60, 60, 0.5)");
+    }
+  }
+}
+
+// function animate() {
+//   //requestAnimationFrame takes another function has argument
+//   requestAnimationFrame(animate);
+//   //clear canvas
+//   c.clearRect(0, 0, innerWidth, innerHeight);
+//   console.log("animate is being called");
+//   for (let i = 0; i < circleArray.length; i++) {
+//     circleArray[i].update();
+//   }
+// }
+// animate();
+init();
+
+//procedurally generate circles using a for loop
+// for (let i = 0; i < 8; i++) {
+//   var x = Math.random() * window.innerWidth;
+//   var y = Math.random() * window.innerHeight;
+//   let colorR = Math.random() * 255;
+//   let colorB = Math.random() * 255;
+//   let colorG = Math.random() * 255;
+//   let colorA = Math.random();
+//   let randomColor = `rgba(${colorR}, ${colorB}, ${colorG}, ${colorA} )`;
+//   c.beginPath();
+//   c.arc(x, y, 30, 0, Math.PI * 2, false);
+//   c.strokeStyle = randomColor;
+//   c.stroke();
+// }
+
+//
