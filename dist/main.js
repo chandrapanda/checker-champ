73b29d946407d5f1239d235092eb4cd18c3bc506
Chandra Holt <caholt@sbgtv.com>
2023-01-06 16:39:50 -0800

    Added pieceLocation object to dynamically render game pieces based on coordinates

--- dist/main.js
+++ dist/main.js
@@ -34,39 +34,92 @@ window.addEventListener("resize", function () {
 });
 
 //object for square on game board
-function Square(x, y, width, height, color) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
+// function Square(x, y, width, height, color) {
+//   this.x = x;
+//   this.y = y;
+//   this.width = width;
+//   this.height = height;
+//   this.color = color;
+//create method within object to draw each square
+//   this.draw = function () {
+//     c.fillStyle = this.color;
+//     c.fillRect(this.x, this.y, this.width, this.height);
+//   };
+// }
+
+//TODO: two different draws : static (game board) & interactive (pieces)
+
+//circle object for gamepieces
+function Circle(x, y, dx, dy, radius, color) {
+  this.xAxis = x;
+  this.yAxis = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.minRadius = radius;
   this.color = color;
-  //create method within object to draw each square
+  //create method within object
   this.draw = function () {
-    c.fillStyle = this.color;
-    c.fillRect(this.x, this.y, this.width, this.height);
+    // Drawing arc / circle
+    c.beginPath();
+    // //arc() takes x, y, radius, startAngle, endAngle, drawCounterClockwise
+    c.arc(this.xAxis, this.yAxis, this.radius, 0, Math.PI * 2, false);
+
+    c.strokeStyle = color;
+    c.fillStyle = color;
+
+    c.stroke();
+    c.fill();
   };
-
-  //this is where interactivity occurs
-  if (
-    mouse.x - this.xAxis < 50 &&
-    mouse.x - this.xAxis > -50 &&
-    mouse.y - this.yAxis < 50 &&
-    mouse.y - this.yAxis > -50
-  ) {
-    if (this.radius < maxRadius) {
-      this.radius += 1;
+  this.update = function () {
+    if (this.xAxis + this.radius > innerWidth || this.xAxis - this.radius < 0) {
+      this.dx = -this.dx;
     }
-  } else if (this.radius > this.minRadius) {
-    this.radius -= 1;
-  }
 
-  this.draw();
+    if (
+      this.yAxis + this.radius > innerHeight ||
+      this.yAxis - this.radius < 0
+    ) {
+      this.dy = -this.dy;
+    }
+    this.xAxis += this.dx;
+    this.yAxis += this.dy;
+
+    //this is where interactivity occurs
+    if (
+      mouse.x - this.xAxis < 50 &&
+      mouse.x - this.xAxis > -50 &&
+      mouse.y - this.yAxis < 50 &&
+      mouse.y - this.yAxis > -50
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius += 1;
+      }
+    } else if (this.radius > this.minRadius) {
+      this.radius -= 1;
+    }
+
+    this.draw();
+  };
 }
+//TODO: create memory object: array within an array to access where on the board the pieces lie
+//(use object like type:basePiece / kingPiece)
+//rows & columns
 
-let squareArray = [];
-
+const pieceLocations = [
+  [{ team: "white" }, {}, {}, {}, {}, {}, {}, { team: "CHAOS" }],
+  [{}, {}, {}, {}, {}, {}, {}, {}],
+  [{}, {}, { team: "white" }, {}, {}, { team: "white" }, {}, {}],
+  [{}, {}, {}, {}, {}, {}, {}, {}],
+  [{}, {}, {}, {}, {}, {}, { team: "black" }, {}],
+  [{}, {}, {}, {}, {}, {}, {}, {}],
+  [{}, {}, {}, {}, {}, {}, {}, {}],
+  [{ team: "Chris" }, {}, {}, {}, {}, {}, {}, { team: "white" }],
+];
+console.log(pieceLocations);
+//TODO: grab center coordinates for each square drawn so gamepiece objects have a reference point
 function createCheckerBoard() {
-  squareArray = [];
+  console.log("createCheckerBoard called");
   let squareSize = canvas.height / 12;
   let topBoardHorizontal = canvas.height / 12;
   let topBoardVertical = canvas.height / 12;
@@ -79,22 +132,71 @@ function createCheckerBoard() {
       let y = topBoardVertical + i * squareSize;
       c.fillRect(x, y, squareSize, squareSize);
     }
+    drawGamePieces(pieceLocations);
   }
-
-  //   for (let i = 0; i < canvas.width; i += 200) {
-  //     for (let j = 0; j < canvas.height; j += 200) {
-  //       let square = new Square(i, j, 100, 100, "rgba(60, 179, 113, 0.5)");
-  //       square.draw();
-  //     }
-  //   }
-  //   for (let i = 100; i < canvas.width; i += 200) {
-  //     for (let j = 100; j < canvas.height; j += 200) {
-  //       let square = new Square(i, j, 100, 100, "rgba(60, 60, 60, 0.5)");
-  //       square.draw();
-  //     }
-  //   }
 }
 
+function drawGamePieces(pieceLocations) {
+  let squareSize = canvas.height / 12;
+  //instantiate player pieces and their coordinates to dynamically render
+  let playerOnePieces = [];
+  let playerTwoPieces = [];
+  //iterate through piece locations and stick them where they should go
+
+  for (let row = 0; row < pieceLocations.length; row++) {
+    for (let column = 0; column < pieceLocations[row].length; column++) {
+      let currentSpace = pieceLocations[row][column];
+      if (currentSpace.team == "white") {
+        //render game pieces according to axes
+        let xCoord = squareSize * (row + 1) + squareSize / 2;
+        let yCoord = squareSize * (column + 1) + squareSize / 2;
+        console.log(xCoord + "y coord " + yCoord);
+        //draw white gamepieces
+        let newGamePiece = c.beginPath();
+        c.arc(xCoord, yCoord, 20, 0, Math.PI * 2, false);
+        c.fillStyle = "white";
+        c.strokeStyle = "black";
+        c.fill();
+        c.stroke();
+        c.closePath();
+        playerOnePieces.push(newGamePiece);
+        console.log(
+          "column " + column + "row " + row + "THERE IS A PLAYER HERE"
+        );
+      } else if (currentSpace.team == "black") {
+        let xCoord = squareSize * (row + 1) + squareSize / 2;
+        let yCoord = squareSize * (column + 1) + squareSize / 2;
+        console.log(xCoord + "y coord " + yCoord);
+        //draw white gamepieces
+        let newGamePiece = c.beginPath();
+        c.arc(xCoord, yCoord, 20, 0, Math.PI * 2, false);
+        c.fillStyle = "black";
+        c.strokeStyle = "white";
+        c.fill();
+        c.stroke();
+        c.closePath();
+        playerOnePieces.push(newGamePiece);
+        console.log(
+          "column " + column + "row " + row + "THERE IS A PLAYER HERE"
+        );
+      }
+    }
+  }
+
+  // console.log("drawGamePieces called");
+
+  // for (let i = 0; i < 12; i++) {
+  //   //draw black gamepieces
+  //   let newGamePiece = c.beginPath();
+  //   c.arc(115 + i * 100, 200 + i * 100, 20, 0, Math.PI * 2, false);
+  //   c.fillStyle = "black";
+  //   c.strokeStyle = "white";
+  //   c.fill();
+  //   c.stroke();
+  //   c.closePath();
+  //   playerTwoPieces.push(newGamePiece);
+  // }
+}
 // let circleArray = [];
 
 // function init() {
